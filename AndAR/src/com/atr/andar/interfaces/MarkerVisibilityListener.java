a10ad15b974cf4e00fe39c6486373408a75532a4
tww <331397964@qq.com>
2014-09-24 17:30:48 +0800

    v3.0 It's all to ATRer

---
+++ AndAR/src/com/atr/andar/interfaces/MarkerVisibilityListener.java
@@ -0,0 +1,10 @@
+package com.atr.andar.interfaces;
+
+/**
+ * gets notified 
+ * @author Tobi
+ *
+ */
+public interface MarkerVisibilityListener {
+	public void makerVisibilityChanged(boolean visible);
+}
